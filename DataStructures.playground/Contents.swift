898c38922a7b24f37a44b42e2390125c55bc5ace
Patrick Frei <p_frei@outlook.com>
2021-11-25 16:48:26 +0100

    lecture 3 finished

--- DataStructures.playground/Contents.swift
+++ DataStructures.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// Stacks -----------------
 
 public struct Stack<Element> {
     
@@ -7,8 +8,16 @@ public struct Stack<Element> {
     
     public init() {}
     
+    public init(_ elements: [Element]) {
+        storage = elements
+    }
+    
     public mutating func push(_ element: Element) {
         storage.append(element)
+    }
+    
+    public mutating func pop() -> Element? {
+        return storage.popLast()
     }
     
 }
@@ -17,3 +26,23 @@ var stack = Stack<Int>()
 stack.push(3)
 stack.push(5)
 
+// Linked Lists -----------------
+// collection of values arranged in a linear, unidirectional sequence
+// "chain of nodes"
+
+public class Node<Value> {
+    public var value: Value
+    public var next: Node?
+    
+    public init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+
+node1.next = node2
+node2.next = node3
